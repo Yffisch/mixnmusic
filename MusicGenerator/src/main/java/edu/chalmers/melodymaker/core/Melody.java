485ab8795363f5d63339ec2503c65f19cc8f81e4
Emma Nyborg <emma.nyborg@live.se>
2014-04-02 10:28:26 +0200

    Added the new class Melody

---
+++ MusicGenerator/src/main/java/edu/chalmers/melodymaker/core/Melody.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.chalmers.melodymaker.core;
+
+/**
+ *
+ * @author PearlOfNash
+ */
+public class Melody {
+    
+    public String genre, titel, noteLegnth, signature;
+}
