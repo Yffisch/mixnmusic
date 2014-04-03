2e5ea1d01c58fcf3a42d114381839b90e5e25ddd
Yffisch <patriarch.k@gmail.com>
2014-04-03 23:28:08 +0200

    Added some test rules

--- MusicGenerator/src/main/java/edu/chalmers/melodymaker/core/MelodyGenerator.java
+++ MusicGenerator/src/main/java/edu/chalmers/melodymaker/core/MelodyGenerator.java
@@ -35,8 +35,8 @@ public class MelodyGenerator {
      */
     public MelodyGenerator() {
         MAX = 100;
-        MIN = 5;
-        ORDER = 1;
+        MIN = 25;
+        ORDER = 2;
         initTables();
     }
 
@@ -128,7 +128,35 @@ public class MelodyGenerator {
     }
 
     public boolean breaksMusicTheory(String prev, String next) {
-        return prev.charAt(prev.length() - 1) == 'A' && next.equals("A");
+        // Här kanske man ska ha en getTonart och getMeter och så to be able to implement what notes that are allowed
+        //does break theory
+        if(prev.charAt(prev.length() - 1) == 'C' && next.equals("C")){
+            return true;
+        }
+        //does break theory
+        else if(prev.charAt(prev.length() - 1) == 'D' && next.equals("D")){
+            return true;
+        }
+         else if(prev.charAt(prev.length() - 1) == 'E' && next.equals("E")){
+            return true;
+        }
+         else if(prev.charAt(prev.length() - 1) == 'F' && next.equals("F")){
+            return true;
+        }
+         else if(prev.charAt(prev.length() - 1) == 'G' && next.equals("G")){
+            return true;
+        }
+         else if(prev.charAt(prev.length() - 1) == 'A' && next.equals("A")){
+            return true;
+        }
+         else if(prev.charAt(prev.length() - 1) == 'B' && next.equals("B")){
+            return true;
+        }
+         else if(prev.charAt(prev.length() - 1) == '2' && next.equals("2")){
+            return true;
+        }
+        
+        return false;
 
     }
 
